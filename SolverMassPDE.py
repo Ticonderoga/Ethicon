2680df80acb740e64e240dd8e1c277c1fc83c692
Ticonderoga <ticonderogafr@gmail.com>
2015-04-16 14:32:08 +0200

    * 1st version with function

--- SolverMassPDE.py
+++ SolverMassPDE.py
@@ -30,6 +30,43 @@ import matplotlib.pyplot as plt
 def sorptionX_aw(a,C,Xm) :
     return Xm*(C*a/(1.+C*a)+a/(1.-a))
     
+def simul_num(Mat,Cond):
+        
+    Xinit=Mat.Mass.X_init*np.ones_like(x)
+    nbsavet=int(Cond.Time.final / Cond.Time.interval_savet)+1
+    
+    SaveX=np.empty((nbsavet,Mat.Size.number))
+    SaveX[0,:]=Xinit
+    
+    Fo=Mat.Mass.diffusivity*Cond.Time.step/dx**2
+    Vect_m=-Fo*np.ones(Mat.Size.number-1)
+    Vect_p=-Fo*np.ones(Mat.Size.number-1)
+    Vect=1+2*Fo*np.ones(Mat.Size.number)
+    Vect[0]=1.
+    Vect_p[0]=0.
+    Vect_m[-1]=-1.
+    Vect[-1]=1.
+    
+    alpha,beta=Ant.Thomas_alpha_beta(Vect_m,Vect,Vect_p)
+#    HR=Cond.Gaz.humidity(time)
+    Xsurf=sorptionX_aw(HRsurf*np.ones_like(time),CL,Xm)
+    n=0    
+    X=Xinit
+    for p in xrange(len(time)-1) :
+        rhs=X
+        rhs[0]=Xsurf[p+1]
+        rhs[-1]=0
+        X=Ant.Thomas_x(Ant.Thomas_y(beta,rhs),alpha,Vect_p)
+        if ((p+1)%int(Cond.Time.interval_savet/Cond.Time.step))==0 :
+            n=n+1
+            print "======================"
+            print "Iteration %d - Temps %.5g" %(p,(p+1)*Cond.Time.step)
+            SaveX[n,:]=X
+    
+    
+    savetime=np.arange(0,nbsavet*Cond.Time.interval_savet,Cond.Time.interval_savet)
+    
+    return savetime,SaveX
 
 if __name__ == '__main__':
     
@@ -58,45 +95,13 @@ if __name__ == '__main__':
     
     x,dx=np.linspace(0,Material.Size.height,Material.Size.number,retstep=True)
     time=np.arange(0,Conditions.Time.final+Conditions.Time.step,Conditions.Time.step)
-    
-    Xinit=Material.Mass.X_init*np.ones_like(x)
-    nbsavet=int(Conditions.Time.final / Conditions.Time.interval_savet)+1
-    
-    SaveX=np.empty((nbsavet,Material.Size.number))
-    SaveX[0,:]=Xinit
-    
-    Fo=Material.Mass.diffusivity*Conditions.Time.step/dx**2
-    Vect_m=-Fo*np.ones(Material.Size.number-1)
-    Vect_p=-Fo*np.ones(Material.Size.number-1)
-    Vect=1+2*Fo*np.ones(Material.Size.number)
-    Vect[0]=1.
-    Vect_p[0]=0.
-    Vect_m[-1]=-1.
-    Vect[-1]=1.
-    
-    alpha,beta=Ant.Thomas_alpha_beta(Vect_m,Vect,Vect_p)
-#    HR=Conditions.Gaz.humidity(time)
+
     Xm=0.03398551582055446
     CL=10.906023000635287
     Xfinal = ts2[-1,1]
     fopt= lambda x : Xfinal-sorptionX_aw(x,CL,Xm)
     HRsurf=sp.optimize.fsolve(fopt,0.2)[0]
-    Xsurf=sorptionX_aw(HRsurf*np.ones_like(time),CL,Xm)
-    n=0    
-    X=Xinit
-    for p in xrange(len(time)-1) :
-        rhs=X
-        rhs[0]=Xsurf[p+1]
-        rhs[-1]=0
-        X=Ant.Thomas_x(Ant.Thomas_y(beta,rhs),alpha,Vect_p)
-        if ((p+1)%int(Conditions.Time.interval_savet/Conditions.Time.step))==0 :
-            n=n+1
-            print "======================"
-            print "Iteration %d - Temps %.5g" %(p,(p+1)*Conditions.Time.step)
-            SaveX[n,:]=X
-    
-    
-    savetime=np.arange(0,nbsavet*Conditions.Time.interval_savet,Conditions.Time.interval_savet)
+    savetime,SaveX = simul_num(Material,Conditions)
     plt.figure(1)
     plt.xlabel(r"Distance $\left[mm\right]$")
     plt.ylabel(r"Water\ Content $\left[kg \cdot m^{-3} \right]$")
